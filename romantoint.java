dfb5eacf7b249d111cf419e8aa96dc0dbd1cbbe2
Syed <syedisaf@gmail.com>
2023-09-22 16:33:31 +0530

    fourth program

---
+++ romantoint.java
@@ -0,0 +1,48 @@
+//Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+//java
+
+class romantoint {
+    public int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int curValue = 0;
+
+            switch (c) {
+                case 'I':
+                    curValue = 1;
+                    break;
+                case 'V':
+                    curValue = 5;
+                    break;
+                case 'X':
+                    curValue = 10;
+                    break;
+                case 'L':
+                    curValue = 50;
+                    break;
+                case 'C':
+                    curValue = 100;
+                    break;
+                case 'D':
+                    curValue = 500;
+                    break;
+                case 'M':
+                    curValue = 1000;
+                    break;
+            }
+
+            if (curValue < prevValue) {
+                result -= curValue;
+            } else {
+                result += curValue;
+            }
+
+            prevValue = curValue;
+        }
+
+        return result;
+    }
+}
