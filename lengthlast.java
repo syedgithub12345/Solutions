90078fdac4a5ccfe6d35015be6b58f435726a9b7
Syed <syedisaf@gmail.com>
2023-09-26 23:59:15 +0530

    eighth program

--- lengthlast.java
+++ lengthlast.java
@@ -1,3 +1,7 @@
+//Given a string s consisting of words and spaces, 
+//return the length of the last word in the string.
+//A word is a maximal substring consisting of non-space characters only.
+
 class lengthlast {
     public int lengthOfLastWord(String s) {
         s = s.trim();
