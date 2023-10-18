8a5b71ec5fb449d78b4347c9683de7a37923872f
Syed <syedisaf@gmail.com>
2023-10-19 00:06:07 +0530

    sixteenth program

--- addbinary.java
+++ addbinary.java
@@ -4,9 +4,24 @@
 
 class addbinary {
     public String addBinary(String a, String b) {
-        int aDecimal = Integer.parseInt(a, 2);
-        int bDecimal = Integer.parseInt(b, 2);
-        int sum = aDecimal + bDecimal;
-        return Integer.toBinaryString(sum);
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(j--) - '0';
+            }
+
+            result.insert(0, sum % 2);
+            carry = sum / 2;
+        }
+
+        return result.toString();
     }
 }
