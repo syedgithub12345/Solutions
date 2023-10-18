5221a974bf78126bf1661a55e4f3ca7352919723
Syed <syedisaf@gmail.com>
2023-10-18 23:52:01 +0530

    fifteenth program

---
+++ addbinary.java
@@ -0,0 +1,12 @@
+//Given two binary strings a and b, 
+//return their sum as a binary string.
+//java
+
+class Solution {
+    public String addBinary(String a, String b) {
+        int aDecimal = Integer.parseInt(a, 2);
+        int bDecimal = Integer.parseInt(b, 2);
+        int sum = aDecimal + bDecimal;
+        return Integer.toBinaryString(sum);
+    }
+}
