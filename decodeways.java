d56339fc24c8060ecc2d032d1c878cbff5baf446
Syed <syedisaf@gmail.com>
2023-10-25 00:00:17 +0530

    twentyone commit

--- decodeways.java
+++ decodeways.java
@@ -10,23 +10,24 @@
 //java
 
 public class decodeways {
-    public static int numDecodings(String s){
-        if(s==null || s.length()==0){
+    public static int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
             return 0;
         }
-        int n=s.length();
-        int[] dp=new int[n+1];
-        dp[0]=1;
-        dp[1]=s.charAt(0)=='0'?0:1;
-        for(int i=2;i<=n;i++){
-            int onedigit=Integer.parseInt(s.substring(i-1,i));
-            int twodigit=Integer.parseInt(s.substring(i-2,i));
-            if(onedigit>=1 && onedigit<10){
-                dp[i]+=dp[i-1];
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 2; i <= n; i++) {
+            int onedigit = Integer.parseInt(s.substring(i - 1, i));
+            int twodigit = Integer.parseInt(s.substring(i - 2, i));
+            if (onedigit >= 1 && onedigit < 10) {
+                dp[i] += dp[i - 1];
             }
-            if(twodigit>=10 && twodigit<=26){
-                dp[i]+=dp[i-2];
+            if (twodigit >= 10 && twodigit <= 26) {
+                dp[i] += dp[i - 2];
             }
-        }return dp[n];
+        }
+        return dp[n];
     }
 }
