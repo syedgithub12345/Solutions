45cf4f0172bb4ebd1f61347537fac849f2e99ec5
Syed Safi <syedisaf@gmail.com>
2024-06-12 00:37:45 +0530

    fifty commit

---
+++ gcd1.java
@@ -0,0 +1,20 @@
+//******************************** */
+//Greatest Common Divisor
+public class gcd1 {
+    public static int gcdIter(int u, int v) {
+        int t;
+        while (v != 0) {
+            t = u;
+            u = v;
+            v = t % v;
+        }
+        return u < 0 ? -u : u;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 6;
+        int result = gcdIter(n, m);
+        System.out.println(result);
+    }
+}
