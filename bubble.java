2218a29a391196b41401133b98597102dab68227
Syed Safi <syedisaf@gmail.com>
2024-06-13 23:04:40 +0530

    fifty three commit

--- bubble.java
+++ bubble.java
@@ -24,22 +24,26 @@ public class bubble {
         }
         System.out.println();
 
-        //improved bubble sort
-        //int[] arr1 = { 3, 4, 54, 546, 646, 41, 5, 87, 0, 97 };
-        int var;
-        int flag = 0;
-        for (int l = 0; l < n - 1 && flag == 1; l++) {
-            flag = 0;
-            for (int m = 0; m < n - 1; m++) {
-                flag = 1;
-                var = arr[m];
-                arr[m + 1] = arr[m];
-                arr[m] = var;
-            }
-        }
-        System.out.println("Implementing improved bubble sort :");
-        for (int p = 0; p < n; p++) {
-            System.out.print(arr[p] + " ");
-        }
+        // improved bubble sort
+        // int[] arr1 = { 3, 4, 54, 546, 646, 41, 5, 87, 0, 97 };
+        // boolean swapped;
+        // for (int l = 0; l < n - 1; l++) {
+        // swapped = false;
+        // for (int m = 0; m < n - 1 - l; m++) {
+        // if (arr[m] > arr[m + 1]) {
+        // int temp1 = arr[m];
+        // arr[m] = arr[m + 1];
+        // arr[m + 1] = temp1;
+        // swapped = true;
+        // }
+        // }
+        // If no two elements were swapped by inner loop, then the array is sorted
+        // if (!swapped)
+        // break;
+        // }
+        // System.out.println("Implementing improved bubble sort :");
+        // for (int p = 0; p < n; p++) {
+        // System.out.print(arr[p] + " ");
+        // }
     }
 }
