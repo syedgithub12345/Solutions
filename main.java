c760865d975f0cc11f43213621263e3b693c4f82
Syed Safi <syedisaf@gmail.com>
2024-06-17 00:04:05 +0530

    fifty nine commit

---
+++ main.java
@@ -0,0 +1,28 @@
+//armstrong
+import java.util.Scanner;
+
+public class main {
+    public static void armstrong1(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter two numbers (intervals): ");
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+
+        System.out.println("Armstrong numbers between " + n1 + " and " + n2 + " are:");
+        for (int i = n1 + 1; i < n2; ++i) {
+            int temp = i;
+            int num = 0;
+            int rem;
+            while (temp != 0) {
+                rem = temp % 10;
+                num += rem * rem * rem;
+                temp /= 10;
+            }
+            if (i == num) {
+                System.out.print(i + " ");
+            }
+        }
+        scanner.close();
+    }
+}
