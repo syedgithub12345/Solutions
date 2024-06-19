f1acd4c671a9234c7c416bee044425f576a99b59
Syed Safi <syedisaf@gmail.com>
2024-06-19 21:03:30 +0530

    sixty two commit

---
+++ findprimefactor.java
@@ -0,0 +1,34 @@
+
+//****************** */
+import java.util.Scanner;
+
+public class findprimefactor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        System.out.println("Prime factors of " + num + " are: ");
+        findPrimeFactors(num);
+        scanner.close();
+    }
+
+    public static void findPrimeFactors(int num) {
+        int i = 1;
+        while (i <= num) {
+            int k = 0;
+            if (num % i == 0) { // Finding factor
+                int j = 1;
+                while (j <= i) { // Check prime or not
+                    if (i % j == 0) {
+                        k++;
+                    }
+                    j++;
+                }
+                if (k == 2) {
+                    System.out.print(i + " is a prime factor\n");
+                }
+            }
+            i++;
+        }
+    }
+}
