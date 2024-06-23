5f9b94cf35a156c7af144d3fdef481b930e880a0
Syed Safi <syedisaf@gmail.com>
2024-06-23 21:00:51 +0530

    fifty eight commit

---
+++ majorityelement.java
@@ -0,0 +1,28 @@
+// element appear n/2 times
+// Boyer-Moore Voting Algorithm
+import java.util.*;
+
+public class majorityelement {
+    public static int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] v = { 3, 3, 3, 4, 5, 54, 54, 54, 3, 3, 3, 3, };
+        System.out.println(majorityElement(v));
+    }
+}
---
+++ reversed1.java
@@ -0,0 +1,30 @@
+//***************************** */
+//reversed
+public class reversed1 {
+    public static void main(String[] args) {
+        String sentence = "jack and jill went up a hill";
+        String reversedSentence = reverseWords(sentence);
+        System.out.println("Original sentence: " + sentence);
+        System.out.println("Reversed sentence: " + reversedSentence);
+    }
+
+    public static String reverseWords(String sentence) {
+        String[] words = sentence.split(" "); // Split the sentence into words
+        StringBuilder reversedSentence = new StringBuilder();
+
+        for (String word : words) {
+            String reversedWord = reverseString(word); // Reverse each word
+            reversedSentence.append(reversedWord).append(" "); // Append reversed word with a space
+        }
+
+        return reversedSentence.toString().trim(); // Remove trailing space and return
+    }
+
+    public static String reverseString(String str) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i)); // Append each character in reverse order
+        }
+        return reversed.toString();
+    }
+}
